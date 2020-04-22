bbcedc3c0459c0e0351e198468908f48f4a889d3
nmalzieu <noe.malzieu@gmail.com>
2020-04-22 20:52:10 +0200

    Add build

---
+++ build/precache-manifest.19103adccd23f84ff28345ee58177032.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68330b296d52148bac7c4572039952a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe5135fa2cf1461cf75",
+    "url": "/static/css/main.b7555f29.chunk.css"
+  },
+  {
+    "revision": "4d9346b3fc400e261e08",
+    "url": "/static/js/2.279514b4.chunk.js"
+  },
+  {
+    "revision": "488607cdd2c37b5d55e5fc5721860d02",
+    "url": "/static/js/2.279514b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe5135fa2cf1461cf75",
+    "url": "/static/js/main.b59ba300.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "9b81497d5dae7d2d2e79e798b8c9fdc0",
+    "url": "/static/media/banlieueSante.9b81497d.png"
+  },
+  {
+    "revision": "2617c650ba84e5521cc7f5fc1c178e8d",
+    "url": "/static/media/carottes.2617c650.svg"
+  },
+  {
+    "revision": "c861e9689a4a41c96d46f79d40fa0068",
+    "url": "/static/media/cookies.c861e968.svg"
+  },
+  {
+    "revision": "aff6a4a1d52b345c61d9b52c76bed3a8",
+    "url": "/static/media/couteau.aff6a4a1.svg"
+  },
+  {
+    "revision": "013059969a991dd5e8809ef6ca8261c3",
+    "url": "/static/media/editable.01305996.svg"
+  },
+  {
+    "revision": "e878d9e3042cc82ba414d2e5cfb16912",
+    "url": "/static/media/facebook.e878d9e3.svg"
+  },
+  {
+    "revision": "b63718efbb74cef7e5506765c57ee78d",
+    "url": "/static/media/fouet1.b63718ef.svg"
+  },
+  {
+    "revision": "26671425d3392f370fefbec3dca5a7d4",
+    "url": "/static/media/leftHand.26671425.png"
+  },
+  {
+    "revision": "01959ea9ea0dd586db2cabd31abfba48",
+    "url": "/static/media/link.01959ea9.svg"
+  },
+  {
+    "revision": "92d9f772e3a5ef81aafc8a19ee32a5ea",
+    "url": "/static/media/logo_ravitailleur.92d9f772.svg"
+  },
+  {
+    "revision": "f2f50094a7741b0361d7a1c8a03f248d",
+    "url": "/static/media/messenger.f2f50094.svg"
+  },
+  {
+    "revision": "569c121a643c5ab5fc58c41fa38960c3",
+    "url": "/static/media/protectionCivile.569c121a.png"
+  },
+  {
+    "revision": "7da8c765acf17fd2ba17ccbffbcfd918",
+    "url": "/static/media/puce-collegue.7da8c765.svg"
+  },
+  {
+    "revision": "a5717d793bb20a143280c0f2e08dc9b3",
+    "url": "/static/media/puce-famille.a5717d79.svg"
+  },
+  {
+    "revision": "c1c164ee16e1b1ffac21b7146555446c",
+    "url": "/static/media/puce-reseau.c1c164ee.svg"
+  },
+  {
+    "revision": "179989a837d0c862758fbb030f99c644",
+    "url": "/static/media/quotes.179989a8.svg"
+  },
+  {
+    "revision": "2442ee9442b37260fae6a7b8d81fc837",
+    "url": "/static/media/restoDuCoeur.2442ee94.svg"
+  },
+  {
+    "revision": "5b3d750a9080bb9872f7706ea96654ad",
+    "url": "/static/media/secoursPop.5b3d750a.svg"
+  },
+  {
+    "revision": "b489d4f7ab9331e380b2e070dc016533",
+    "url": "/static/media/spatule.b489d4f7.svg"
+  },
+  {
+    "revision": "5e574f04fc5a9fbfd27042afee39f439",
+    "url": "/static/media/successLeftHand.5e574f04.svg"
+  },
+  {
+    "revision": "8b78f375607aa2aea026658422c19f95",
+    "url": "/static/media/successRightHand.8b78f375.svg"
+  },
+  {
+    "revision": "55145f70abfa33069ec6577a292e4997",
+    "url": "/static/media/tomate.55145f70.svg"
+  },
+  {
+    "revision": "cddc1151c50f69ad0fc9aa78082e588a",
+    "url": "/static/media/tomateCarotte.cddc1151.png"
+  },
+  {
+    "revision": "da1e29cb2c42028980e6bf25bc719fd9",
+    "url": "/static/media/twitter.da1e29cb.svg"
+  },
+  {
+    "revision": "dc8e6e2877caa3115fb8c0bbb8c61474",
+    "url": "/static/media/whatsapp.dc8e6e28.svg"
+  },
+  {
+    "revision": "5db4b4457ea79daea96ce13b571a2bf3",
+    "url": "/static/media/whitePen.5db4b445.svg"
+  }
+]);
